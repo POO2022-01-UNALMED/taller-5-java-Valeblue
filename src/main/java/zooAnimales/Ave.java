805c8aec5b26b2f632e3c347db8e1bb135182da1
Valeblue <101608411+Valeblue@users.noreply.github.com>
2022-04-28 22:52:57 -0500

    :+1:
    Taller05

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,4 +1,60 @@
 package zooAnimales;
 
 public class Ave {
+import java.util.ArrayList;
+
+
+    public class Ave extends Animal {
+        private static ArrayList<Ave> listado = new ArrayList<Ave>();
+        public static int halcones;
+        public static int aguilas;
+        private String colorPlumas;
+
+        public Ave(){
+            super();
+            Ave.listado.add(this);
+        }
+        public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+            // Atributos que son heredados
+            super(nombre,edad,habitat,genero);
+            // Atributos nuevos en la clase Mamifero
+            this.colorPlumas = colorPlumas;
+            // Cuando se crea el Mamifero se espera que se anada al listado
+            Ave.listado.add(this);
+        }
+
+        // Metodos get y set para los atributos normales de Ave
+        public String getColorPlumas() {
+            return this.colorPlumas;
+        }
+        public void setColorPlumas(String colorPlumasNuevo) {
+            this.colorPlumas = colorPlumasNuevo;
+        }
+
+        // Metodos
+
+        //
+        public static Ave crearHalcon(String nombre, int edad, String genero) {
+            Ave nuevoHalcon = new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+            halcones += 1;
+            return nuevoHalcon;
+        }
+
+        public static Ave crearAguila(String nombre, int edad, String genero) {
+            Ave nuevoAguila = new Ave(nombre,edad,"montanas",genero,"blanco");
+            aguilas += 1;
+            return nuevoAguila;
+        }
+
+        // Metodos get para la lista de Aves
+        public static int cantidadAves(){
+            return listado.size();
+        }
+
+        public String movimiento() {
+            return "volar";
+        }
+
+
+    }
 }
--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,4 +1,60 @@
 package zooAnimales;
 
-public class Reptil {
+import java.util.ArrayList;
+
+public class Reptil extends Animal {
+    private static ArrayList<Reptil> listado = new ArrayList<>();
+    public static int iguanas;
+    public static int serpientes;
+    private String colorEscamas;
+    private int largoCola;
+
+    public Reptil() {
+        super();
+        Reptil.listado.add(this);
+    }
+    public Reptil(String nombre, int edad, String habitat, String genero,String colorEscamas, int largoCola) {
+        // Para atributos heredados
+        super(nombre,edad,habitat,genero);
+        // Para atributos de la clase
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+        // Para agregar a la lista de Reptiles cuando este se cree
+        Reptil.listado.add(this);
+    }
+    //Metodos get y set
+    public String getColorEscamas() {
+        return this.colorEscamas;
+    }
+    public void setColorEscamas(String colorEscamasNuevo) {
+        this.colorEscamas = colorEscamasNuevo;
+    }
+
+    public int getLargoCola() {
+        return this.largoCola;
+    }
+    public void setLargoCola(int largoColaNuevo) {
+        this.largoCola = largoColaNuevo;
+    }
+
+    public static Reptil crearIguana(String nombre, int edad, String genero) {
+        Reptil nuevoIguana = new Reptil(nombre,edad,"humedal",genero,"verde",3);
+        iguanas += 1;
+        return nuevoIguana;
+    }
+
+    public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+        Reptil nuevoSerpiente = new Reptil(nombre,edad,"jungla",genero,"blanco",1);
+        serpientes += 1;
+        return nuevoSerpiente;
+    }
+    // Metodos get para la lista de Reptiles
+    public static int cantidadReptiles(){
+        return listado.size();
+    }
+    // Moverse
+    public String movimiento() {
+        return "reptar";
+    }
+
 }
